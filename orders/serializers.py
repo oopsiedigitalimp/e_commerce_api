6b43b74a17b8a18d4982ba7b5e6f612143f18da5
oopsiedigitalimp <vgorshkov345@gmail.com>
2025-06-02 23:05:17 +0200

    Updated order-creation view, made validation through serializer.

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
 from products.serializers import CustomerProductSerializer
+from users.models import User
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product = CustomerProductSerializer()
@@ -29,4 +30,61 @@ class OrderSerializer(serializers.ModelSerializer):
             'last_updated_at',
             'items',
             'total_price',
-        )    
+        )
+
+class CreateOrderSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+    shipping_address = serializers.CharField()
+    comment = serializers.CharField(required=False, allow_blank=True)
+
+    def validate(self, data):
+        request = self.context['request']
+        cart = request.cart
+
+        if not cart.items.exists():
+            raise serializers.ValidationError("Cart is empty.")
+        
+        for item in cart.items.all():
+            if item.quantity > item.product.quantity:
+                raise serializers.ValidationError({
+                "error": "Недостаточно товара на складе",
+                "product": item.product.name,
+                "available": item.product.quantity,
+                "requested": item.quantity
+            })
+
+        return 
+    
+    def create(self, validated_data):
+        request = self.context['request']
+        cart = request.cart
+
+        if request.user.is_authenticated:
+            user = cart.user
+        else:
+            user = None
+
+        order = Order.objects.create(
+            user=user,
+            customer_first_name=validated_data.get('first_name'),
+            customer_last_name=validated_data.get('last_name'),
+            customer_email=validated_data.get('email'),
+            shipping_address=validated_data.get('shipping_address'),
+            comment=validated_data.get('comment', ''),
+            total_price=cart.get_total,
+        )
+
+        for item in cart.items.all():
+            OrderItem.objects.create(
+                order=order,
+                product=item.product,
+                quantity=item.quantity
+            )
+
+        cart.clear()
+        cart.is_active = False
+        cart.save()
+
+        return order
--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,7 @@ from users.models import User
 from products.models import Product
 from products.permissions import IsAdminOrReadOnly
 from .mixins import OrderQuerysetRoleBasedMixin
-from .serializers import OrderSerializer, OrderItemSerializer
+from .serializers import OrderSerializer, OrderItemSerializer, CreateOrderSerializer
 from .models import Order, OrderItem
 
 class OrderListAPIView(OrderQuerysetRoleBasedMixin, generics.ListAPIView):
@@ -20,54 +20,7 @@ class OrderRetrieveUpdateDestroyAPIView(OrderQuerysetRoleBasedMixin, generics.Re
 
 class OrderCreateAPIView(APIView):
     def post(self, request):
-        cart = request.cart
-        
-        if cart.items.exists():
-            for item in cart.items.all():
-                if item.quantity > item.product.quantity:
-                    return Response({
-                    "error": "Недостаточно товара на складе",
-                    "product": item.product.name,
-                    "available": item.product.quantity,
-                    "requested": item.quantity
-                }, status=status.HTTP_400_BAD_REQUEST)
-
-            if request.user.is_authenticated:
-                user = cart.user
-            else:
-                user, created = User.objects.get_or_create(
-                    email=request.data.get('email'),
-                    defaults={
-                        "first_name": request.data.get("first_name", ""),
-                        "last_name": request.data.get("last_name", ""),
-                        "role": "Customer",
-                    }
-                )
-                
-            try:
-                order = Order.objects.create(
-                        user=user,
-                        customer_first_name=user.first_name,
-                        customer_last_name=user.last_name,
-                        customer_email=user.email,
-                        shipping_address=request.data.get('shipping_address'),
-                        comment=request.data.get('comment'),
-                        total_price=cart.get_total,
-                    )
-            except Exception as e:
-                return Response({"error": f"Can't create order: {str(e)}"}, status=400)
-
-            for item in cart.items.all():
-                OrderItem.objects.create(
-                    order=order,
-                    product=item.product,
-                    quantity=item.quantity,
-                )
-
-            cart.clear()
-            cart.is_active = False
-            cart.save()
-
-            serializer = OrderSerializer(order)
-
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer = CreateOrderSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
